5b223687fe54074321f21fa95809455d9c028540
amartineau <amartineau@unareil.eu>
2018-02-14 11:29:28 +0100

    ajout des getter setter et des constructeurs

--- ProjetMineraux/src/eu/unareil/mineraux/bo/Commune.java
+++ ProjetMineraux/src/eu/unareil/mineraux/bo/Commune.java
@@ -3,13 +3,60 @@ import java.util.ArrayList;
 
 public class Commune {
 
-  private Integer id;
+private Integer id;
+private String nom;
+private Integer codePostal;
+private Pays lePays;
+private ArrayList <Gite>  lesGites= new ArrayList<>();
 
-  private String nom;
 
-  private Integer codePostal;
+public Commune(Integer id, String nom, Integer codePostal, Pays lePays) {
+	this.setId(id);
+	this.setNom(nom);
+	this.setCodePostal(codePostal);
+	this.setLePays(lePays);
+}
 
-    private Pays lePays;
-  private ArrayList  lesGites;
+public Commune(String nom, Integer codePostal, Pays lePays) {
+	this(null, nom, codePostal, lePays);
+}
 
+public Integer getId() {
+	return id;
+}
+public void setId(Integer id) {
+	this.id = id;
+}
+public String getNom() {
+	return nom;
+}
+public void setNom(String nom) {
+	this.nom = nom;
+}
+public Integer getCodePostal() {
+	return codePostal;
+}
+public void setCodePostal(Integer codePostal) {
+	this.codePostal = codePostal;
+}
+public Pays getLePays() {
+	return lePays;
+}
+public void setLePays(Pays lePays) {
+	this.lePays = lePays;
+}
+public ArrayList<Gite> getLesGites() {
+	return lesGites;
+}
+public void setLesGites(ArrayList<Gite> lesGites) {
+	this.lesGites = lesGites;
+}
+public void ajouteGite(Gite unGite)
+{
+	this.lesGites.add(unGite);
+}
+public void supprimeGite(Gite unGite)
+{
+	this.lesGites.remove(unGite);
+}
 }
